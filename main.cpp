fc51e2121559961669f9d6eff62dd471f8d429b4
land20150303 <land20150303@126.com>
2015-12-04 06:52:15 +0800

    gggg

--- main.cpp
+++ main.cpp
@@ -1,13 +1,44 @@
 #include <stdio.h>
 
 int main() {
-    int a = 5,b = 0x19,c = 010;//怎么又在这里犯了符号的错
-    if(a + !a < a + !c){
-        printf("yes! \n");
-    }else
-    if(c + c > b)
-        printf("wrong! \n");
-    printf("no! \n");
+    /**
+     * 28题
+     */
+//    int a = 5,b = 0x19,c = 010;//怎么又在这里犯了符号的错
+//    if(a + !a < a + !c){
+//        printf("yes! \n");
+//    }else
+//    if(c + c > b)
+//        printf("wrong! \n");
+//    printf("no! \n");
 
-    return 0;
+    /**
+     * 29题
+     */
+//    int a[3][4] = {1,3,5,7,12,14,16,18,9,6,4,2},j,k,m,n,x;x=m=n=0;
+//    for (j = 2; j > -1 ; j--)
+//        for(k=0;k<4;k++)
+//            if(x<a[j][k])
+//                x=a[j][k],m=j,n=k;
+//    printf("a[%d][%d] = %d\n",m,n,x);
+
+    /**
+     * 32题
+     */
+//    float x,max,min;
+//    scanf("%f",&x);
+//    max=min=x;
+//    while (x>0){
+//        if(x>max) max = x ;
+//        if(x<min) min = x;
+//        scanf("%f",&x);
+//    }
+//
+//    printf("\n max = %f \n min = %f \n",max,min);
+//
+//    return 0;
+
+
+
+
 }
